7e0d52e11717e89a63d00f5456c55a45c887e9b7
Jevgeņijs Zaharovs <officialmrjz@gmail.com>
2020-02-06 22:22:10 +0200

    Izveidots tresais uzdevums un pieliktas console.log rindas

--- JSBEGINNIN.js
+++ JSBEGINNIN.js
@@ -9,7 +9,7 @@
 function tekstaApgriesana(str) {
     return str.split("").reverse().join("");
 }
-tekstaApgriesana("JEVGENIJS ZAHAROVS");
+console.log(tekstaApgriesana("JEVGENIJS ZAHAROVS"));
 // rezultats :  'SVORAHAZ SJINEGVEJ'
 
 /////////// UZDEVUMS 1 \\\\\\\\\\\\
@@ -20,7 +20,18 @@ function salidzinasana(num1, num2) {
         return false;
     }
 }
-salidzinasana(5,1);
-salidzinasana(2,5);
-salidzinasana(4,1);
-salidzinasana(3,4);
+console.log(salidzinasana(5,1) + "  5/1");
+console.log(salidzinasana(2,5)+ "  2/5");
+console.log(salidzinasana(4,1)+ "  4/1");
+console.log(salidzinasana(3,1)+ "  3/1");
+
+/////////// UZDEVUMS 1 \\\\\\\\\\\\
+function arycount(array) {
+    var count=0;
+    for(var i = 0; i < array.length; ++i){
+       count = count+array[i];
+    }
+    return count;
+}
+
+console.log(arycount([1,2,3,4,5])+ " 1,2,3,4,5");
