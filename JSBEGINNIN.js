17d98f883261ee9fed137750e80f183fa21b1c5a
Jevgeņijs Zaharovs <officialmrjz@gmail.com>
2020-02-06 22:05:28 +0200

    Ievietota basic pamaciba

--- JSBEGINNIN.js
+++ JSBEGINNIN.js
@@ -1,3 +1,10 @@
+/// Parbaudit failu izmaiņas   -- git status
+//  Ievietot visus failus -- git add .
+//  Ievietot commentāru  
+//  par tapšanas brīdi --  git commit -m "RAKSTI KOMENTU ŠEIT"
+//  Ievietot šo internetā -- git push
+
+//----------------------------
 /////////// UZDEVUMS 1 \\\\\\\\\\\\
 function tekstaApgriesana(str) {
     return str.split("").reverse().join("");
